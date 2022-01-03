0829a95f73dbd75eb1da8a8b995eb11dc0042068
Ryan Morris <ryansmorris@hotmail.com>
2022-01-03 11:14:24 +1000

    More routing fixes

--- react_portfolio/src/App/App.js
+++ react_portfolio/src/App/App.js
@@ -16,10 +16,10 @@ function App() {
       <PortfolioHeader />
       <Router>
         <Routes>
-          <Route exact path="/" element={<HomeBody/>} />
-          <Route exact path="/projects" element={<ProjectCard/>} />
-          <Route exact path="/resume" element={<Resume/>} />
-          <Route exact path="/contact" element={<ContactPage/>} />
+          <Route exact path="/"><HomeBody /> <Route />
+          <Route exact path="/projects"> <ProjectCard/> <Route />
+          <Route exact path="/resume"> <Resume/> <Route />
+          <Route exact path="/contact"> <ContactPage/> <Route />
         </Routes>
       </Router>
       <Footer />
